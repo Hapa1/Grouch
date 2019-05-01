0ebd51cdd362e13563e5b37d780d71432f742d80
Hapa1 <ryanmfalcons@gmail.com>
2019-05-01 14:41:28 -0700

    configured heroku

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ mongoose.connection.once('open', () => {
     console.log('connected to MongoDB')
 })
 app.use('/graphql',graphqlHTTP({
-    schema, //schema: schema
+    schema, //schema: schema --> schema
     graphiql: true
 }));
 
@@ -33,12 +33,12 @@ if (process.env.NODE_ENV === 'production') {
     //it doesn't recognize the route
     const path = require('path');
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, 'client!', 'build', 'index.html'));
     });
   
   }
 
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log("Server online at port: "+PORT)
