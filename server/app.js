13ed2a546dfb60c8d2f7acea253d9d859ae73d58
Hapa1 <ryanmfalcons@gmail.com>
2019-05-01 14:56:57 -0700

    configured heroku

--- server/app.js
+++ server/app.js
@@ -27,13 +27,13 @@ if (process.env.NODE_ENV === 'production') {
     //Express will server up production assets
     //like main.js file, or main.css
   
-    app.use(express.static('../client/build'));
+    app.use(express.static('../client'));
   
     //Express will serve up the index.html file if
     //it doesn't recognize the route
     const path = require('path');
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+      res.sendFile(path.resolve(__dirname, '../client', 'index.html'));
     });
   
   }
